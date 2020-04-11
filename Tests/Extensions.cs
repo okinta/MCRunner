be0ffa94716a561e515f8bbb5a8cd6bc5e73c670
Robert Roose <robert@shivercube.com>
2020-04-11 10:48:18 -0300

    Added Extensions and MyStrategy classes for tests

---
+++ Tests/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions.Events;
+using FluentAssertions.Execution;
+using System.Linq;
+
+namespace Tests
+{
+    /// <summary>
+    /// Extends classes.
+    /// </summary>
+    internal static class Extensions
+    {
+        /// <summary>
+        /// Asserts that only one event was raised.
+        /// </summary>
+        /// <param name="eventRecorder">The IEventRecorder to check.</param>
+        /// <returns>The IEventRecorder instance for chaining.</returns>
+        public static IEventRecorder OnlyOnce(this IEventRecorder eventRecorder)
+        {
+            var count = eventRecorder.Count();
+            if (count != 1)
+            {
+                Execute.Assertion.FailWith(
+                    "Expected only 1 event to be raised, but found {0}.", count);
+            }
+
+            return eventRecorder;
+        }
+    }
+}
---
+++ Tests/MyStrategy.cs
@@ -0,0 +1,44 @@
+﻿using PowerLanguage;
+using PowerLanguage.Strategy;
+
+namespace Tests
+{
+    class MyStrategy : SignalObject
+    {
+        [Input]
+        public bool CreateCalled { get; set; } = false;
+
+        [Input]
+        public bool StartCalcCalled { get; set; } = false;
+
+        [Input]
+        public bool CalcBarCalled { get; set; } = false;
+
+        public double CurrentHigh
+        {
+            get
+            {
+                return Bars.HighValue;
+            }
+        }
+
+        public MyStrategy(object _ctx) : base(_ctx)
+        {
+        }
+
+        protected override void Create()
+        {
+            CreateCalled = true;
+        }
+
+        protected override void StartCalc()
+        {
+            StartCalcCalled = true;
+        }
+
+        protected override void CalcBar()
+        {
+            CalcBarCalled = true;
+        }
+    }
+}
